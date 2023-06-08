e830046d22a1b10df23f51bad6c85309e31340d4
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-06-08 07:47:18 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 
 import sys
+lens = len(sys.argv)
+if __name__ == '__main__':
 
-print(len(sys.argv) - 1, "arguments:")
+        if len(sys.argv) == 2:
+                print(lens - 1, "argument:")
+                print("1:",sys.argv[1])
+                exit(1)
+                
+        elif lens == 1:
+                print(lens - 1, "arguments:")
+                exit(1)
 
-for i, arg in enumerate(sys.argv[1:]):
-    print("{}: {}".format(i + 1,arg))
+        else:
+                print(lens - 1, "arguments:")
+                for i, arg in enumerate(sys.argv[1:]):
+                        print("{}: {}".format(i + 1,arg))
