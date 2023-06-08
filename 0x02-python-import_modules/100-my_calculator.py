0a544fe7299a22418e45e543ee2c77ef1bd50250
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-06-08 13:48:25 -0700

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add sub mul div
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        a = int(sys.argv[1])
+        op = sys.argv[2]
+        b = int(sys.argv[3])
+
+        if op == '+':
+            result = add(a, b)
+        elif op == '-':
+            result = sub(a, b)
+        elif op == '*':
+            result = mul(a, b)
+        elif op == '/':
+            result = div(a, b)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+
+        print("{} {} {} = {}".format(a, op, b, result))
+    else:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
