2aeabc02f0b8c6878192a4b07c87f4b3ecdfd4a2
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-06-08 09:12:08 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3                                                                                                                                                      
+#!/usr/bin/python3
+
 import sys
 
 total = 0
