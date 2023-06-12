c113a63f17ac8a15f5b4f2c467252bf4fb26c305
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-06-12 11:25:31 -0700

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,11 +1,15 @@
 #include "lists.h"
-
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to the head
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
 int is_palindrome(listint_t **head)
 {
 listint_t *slow = *head, *fast = *head;
 int stack[2048], top = 0;
 
-// Push the first half of the list onto the stack
+/* Push the first half of the list onto the stack */
 while (fast && fast->next)
 {
 stack[top++] = slow->n;
@@ -13,17 +17,17 @@ slow = slow->next;
 fast = fast->next->next;
 }
 
-// If the list has an odd number of elements, skip the middle element
+/* If the list has an odd number of elements, skip the middle element */
 if (fast)
 slow = slow->next;
 
-// Compare the second half of the list with the first half (in reverse order)
+/* Compare the second half of the list with the first half (in reverse order) */
 while (slow)
 {
 if (slow->n != stack[--top])
-return 0;
+return (0);
 slow = slow->next;
 }
 
-return 1;
+return (1);
 }
