836e2248dd218186514e6ff7c1a2b41947fe4692
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-06-15 08:18:28 -0700

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    re = list(a_dictionary.keys())[0]
+    bi = a_dictionary[re]
+    for i, j in a_dictionary.items():
+        if j > big:
+            bi = j
+            re = i
+    return (re)
