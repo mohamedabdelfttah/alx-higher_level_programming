c1c229be4a9bc7c84e42b78b8d1a863505dcbf61
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-06-15 08:08:05 -0700

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = a_dictionary.copy()
-    new_dict[key] = value
+    if a_dictionary:
+        new_dict = a_dictionary.copy()
+        new_dict[key] = value
     return new_dict
