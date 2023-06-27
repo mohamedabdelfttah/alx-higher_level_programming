54892a8874f7207899c06a215741d5c366c2f030
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-06-27 16:28:21 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Defines a class Square"""
 
-class Square:
-    """Class that defines properties of square """
-    pass
 
+class Square:
+
+    """
+    Class that defines properties of square
+    """
+    pass
