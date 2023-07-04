14838d6813a5b1187e43874c0fa36d176fddf9b4
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-07-03 19:28:14 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,6 +22,14 @@ class Rectangle:
             height: An integer to object height.
 
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
